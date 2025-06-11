26593ee8b219e6d4b86492576095a9102e91f5e0
M Rajesh Nayak <mrajeshnayak@Ms-MacBook-Pro.local>
2025-06-11 00:29:35 -0400

    worked on database - MongoDB

--- server/app.js
+++ server/app.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Routes
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected successfully 👍🏻!"))
+  .catch((err) => console.log(err));
+
+
 app.use("/api", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("🎯 Welcome to Budget Tracker!");
-  console.log("🎯 Welcome to Budget");
+  
 });
 
 module.exports = app;
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,44 +1,81 @@
+const jwt = require('jsonwebtoken')
 const users = require('../models/userModel');
+const bcrypt = require('bcrypt');
 
-const loginUser = (req, res) => {
-  console.log('Login attempt:', req.body);
-  const { email, password } = req.body;
+const generateToken = (userId)=> {
+  return jwt.sign({ id:userId }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN || '1d',
+  });
+}
 
-  const user = users.find(user => user.email === email && user.password === password);
+const loginUser = async (req, res) => {
+  try {
+    console.log('Login attempt:', req.body);
+    const { email, password } = req.body;
 
-  if (user) {
+    const user = await users.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+    const token = generateToken(user._id);
+    console.log('Token:', token);
     return res.status(200).json({
       message: 'Login successful',
-      token: 'fake-jwt-token',
-      user
+      token,
+      user: { name: user.name, email: user.email }
     });
-  } else {
-    return res.status(401).json({ message: 'Invalid credentials' });
+
+  } catch (error) {
+    console.error('Error during login:', error);
+    return res.status(500).json({ message: 'Internal server error' });
   }
 };
 
-const signupUser = (req, res) => {
-  console.log('Signup attempt:', req.body);
-  const { name, email, password, confirmPassword } = req.body;
+const signupUser = async (req, res) => {
+  try {
+    console.log('Signup attempt:', req.body);
+    const { name, email, password, confirmPassword } = req.body;
 
-  if (password !== confirmPassword) {
-    return res.status(400).json({ message: 'Passwords do not match' });
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: 'Passwords do not match' });
+    }
+
+    const existingUser = await users.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+ 
+    const hashedPassword = await bcrypt.hash(password, 10); // 10 = salt rounds
+
+    
+    const newUser = new users({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    await newUser.save();
+
+    const token = generateToken(newUser._id);
+
+    return res.status(201).json({
+      message: "Signup successful",
+      token,
+      user: { name, email } 
+    });
+
+  } catch (error) {
+    console.error('Error during signup:', error);
+    return res.status(500).json({ message: 'Internal server error' });
   }
-
-  const existingUser = users.find(user => user.email === email);
-  if (existingUser) {
-    return res.status(400).json({ message: 'User already exists' });
-  }
-
-  const user = { name, email, password };
-  users.push(user);
-
-  return res.status(201).json({
-    message: 'Signup successful',
-    token: 'dummytokenforsignup',
-    user
-  });
 };
+
 
 module.exports = {
   loginUser,
