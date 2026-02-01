7879f2fbead4d53143689bfefdcf7795a58af62a
Rajesh Nayak <mrajesh.nayak24@gmail.com>
2026-02-01 08:30:26 -0500

    Intentionally broke test fix

--- server/tests/app.test.js
+++ server/tests/app.test.js
@@ -4,6 +4,6 @@ const app = require("../app");
 describe("Basic API Test", () =>{
     it("should respond with status 200 on root endpoint", async()=>{
         const res = await request(app).get("/");
-        expect(res.statusCode).toBe(500);
+        expect(res.statusCode).toBe(200);
     })
 })
