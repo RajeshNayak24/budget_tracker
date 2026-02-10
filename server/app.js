e9cee22b83c1c905746c9dd8aab81daaa551f5d2
Rajesh Nayak <mrajesh.nayak24@gmail.com>
2026-02-10 10:23:09 -0500

    CORS Test cases fix

--- server/app.js
+++ server/app.js
@@ -2,17 +2,28 @@ const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const transactionRoutes = require("./routes/transactionRoutes");
-const plaidRoutes = require('./routes/plaidRoutes');
+const plaidRoutes = require("./routes/plaidRoutes");
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://budget-tracker-frontend-vhrh.onrender.com",
+];
+
 app.use(
   cors({
-    origin: "https://budget-tracker-frontend-vhrh.onrender.com",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
-  })
+  }),
 );
 
 app.options("*", cors());
@@ -20,12 +31,11 @@ app.options("*", cors());
 app.use(express.json());
 
 app.use("/api", authRoutes);
-app.use("/api", transactionRoutes)
-app.use("/api/plaid", plaidRoutes)
+app.use("/api", transactionRoutes);
+app.use("/api/plaid", plaidRoutes);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Budget Tracker!");
-  
 });
 
 module.exports = app;
